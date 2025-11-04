d4e0563107fc260f9f901856c2f8caf0253ffdc5
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2025-11-04 12:17:37 +0100

    update createnewuser

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,7 +4,6 @@ namespace App\Actions\Fortify;
 
 use App\Models\User;
 use App\Enums\Role;
-use App\Enums\Role;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
