ba262172c89f83d8acb5bf41639ce25124a4e0a3
SalvadorGriaule <morales.quentin@gmx.fr>
2025-11-10 12:34:40 +0100

    bouton supprime

--- resources/js/components/DisplayList.vue
+++ resources/js/components/DisplayList.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-
+import axios from "axios";
 import { Ref, ref, watchEffect, toValue } from "vue";
+import { destroy } from "@/actions/App/Http/Controllers/ProductController";
 
-const useFetch = (url: string) => {
+const useFetch = (url: string, method: "get" | "post" | "delete" | "put" = "get") => {
     const data: Ref<any> = ref(null);
     const error: Ref<unknown | null> = ref(null);
 
@@ -13,7 +14,7 @@ const useFetch = (url: string) => {
         const urlValue = toValue(url);
 
         try {
-            const res = await fetch(urlValue, { mode: 'cors', credentials: 'same-origin' });
+            const res = await fetch(urlValue, { mode: 'cors', credentials: 'same-origin', method: method });
             data.value = await res.json();
         } catch (e) {
             error.value = e
@@ -23,29 +24,43 @@ const useFetch = (url: string) => {
     return { data, error }
 }
 
-const props = defineProps<{url:string}>()
+const deleteProd = async (id: string) => {
+    if (id) {
+        const del = await axios.delete(destroy(id).url);
+        if (del.data.result) document.querySelector(`#prod-${id}`)?.remove();
+    }
+}
+
+const props = defineProps<{ url: string, mode: "read" | "edit" }>()
 
 let product = useFetch(props.url)
-console.log(product);
+
 </script>
 
 <template>
     <div class="flex flex-wrap">
-        <div v-for="value in product.data.value" class="w-[47.5%] m-2 p-2">
-            <div class="flex justify-between mb-2">
-                <h3 class="text-2xl">{{ value.name }}</h3>
-                <p>{{ new Date(value.created_at).toLocaleDateString("fr-FR", {
-                    day: "numeric", weekday: "short", month:
-                    "long" }) }}
-                </p>
-            </div>
-            <div class="flex p-1 space-x-2">
-                <div class="w-1/4">
-                    <img :src="value.image_path" alt="">
+        <div v-for="value in product.data.value" class="w-[47.5%] m-2 p-2 dark:text-white" :id="'prod-' + value.id">
+            <div>
+                <div v-if="mode == 'edit'" class="flex">
+                    <button @click="deleteProd(value.id)"
+                        class="bg-red-600 p-2 rounded-2xl text-red-200 hover:bg-red-200 hover:text-red-600 duration-50">delete</button>
                 </div>
-                <div>
-                    <p>{{ value.descritpion }}</p>
-                    <p>{{ value.price }} €</p>
+                <div class="flex justify-between mb-2 ">
+                    <h3 class="text-2xl">{{ value.name }}</h3>
+                    <p>{{ new Date(value.created_at).toLocaleDateString("fr-FR", {
+                        day: "numeric", weekday: "short", month:
+                            "long"
+                    }) }}
+                    </p>
+                </div>
+                <div class="flex p-1 space-x-2">
+                    <div class="w-1/4">
+                        <img :src="value.image_path" alt="">
+                    </div>
+                    <div>
+                        <p>{{ value.descritpion }}</p>
+                        <p>{{ value.price }} €</p>
+                    </div>
                 </div>
             </div>
         </div>
